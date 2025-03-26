72251b186c99b2fa21f440070b55d8cf092e921d
Ken <ken.balvant.patel@gmail.com>
2025-03-25 22:10:09 -0400

    Final commit

--- Discord Bot/memebot.py
+++ Discord Bot/memebot.py
@@ -2,21 +2,27 @@ import discord
 import requests
 import json
 
-""" This method is called when the bot has successfully connected to Discord and is ready to start interacting with the API."""
+# update the url to https://meme-api.com/gimme
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+#This method is called when the bot has successfully connected to Discord and is ready to start interacting with the API.
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print(f'Logged on as {self.user}')
+#Reading and responding to messages
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+# create intents keyword argument
 intents = discord.Intents.default()
 intents.message_content = True
 
-"""Starting client and authenticating with Discord"""
+#Starting client and authenticating with Discord"""
 client = MyClient(intents=intents)
-client.run('Your Token Here')
-
-"""Reading and responding to messages"""
-async def on_message(self, message):
-    if message.author == self.user:
-        return
-    
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+client.run('Your Token here')
